d39ce31eaef363fc7e9f7f990cc4877d66fb3ffa
Morgan_laptop <morgansstuart243@gmail.com>
2013-03-02 15:56:24 -0500

    Added accept and receive functions
    Also, added deconstructor definition and private data members to
    supplement other added features (workingSock for accepts).

--- NetComm.h
+++ NetComm.h
@@ -28,22 +28,28 @@ class NetComm
         int queueLength = 10;
         int scok_fd,
                   n,
-           SocketFD;
+           SocketFD,
+        workingSock;
 
         struct sockaddr_in sockAddrIn;
+        socklen_t sockLength;
 
         int SetupSock();
 
     public:    
         NetComm();
+        NetComm(int sockFD);
         NetComm(bool isListener, std::string addr,  unsigned int port);
         ~NetComm();
 
         void Connect();
+
         void Listen();
         void Listen(int connBacklog);
+        int Accept();
 
         void Send(unsigned char *data, int size);
+        int Receive(unsigned char *buffer, int size);
 
         //TODO
         //Receive
